5742cc4207a5e1cac292304efbaa294d8c0e00ec
root <root@rails.(none)>
2012-09-05 17:24:01 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rogapptest::Application.config.secret_token = '8cda0c0905992cb9c45fe54ded6772147c0aa2e6de8108b06429b1774e5f6fbea4de502456bf4f3f592c6b95bfb7f5999fc1043f06abb191678dafb2cde7973e'
